4b34348d140b881e5666fb03b6bc2b577ae8a9ec
Philipp Arndt <2f.mail@gmx.de>
2021-03-13 15:31:18 +0100

    extract changes from change log

---
+++ .github/workflows/getChanges.js
@@ -0,0 +1,28 @@
+let fs = require('fs')
+
+let version = require('../../package.json').version
+
+try {
+    let data = fs.readFileSync('../../CHANGELOG.md', 'utf8')
+
+    let found = false
+    let changes = []
+
+    for (line of data.split(/\r\n|\n/)) {
+        if (line.trim().startsWith("#")) {
+            found = false;
+        }
+
+        if (found) {
+            changes.push(line)
+        }
+
+        if (line.trim().startsWith(`# ${version}`)) {
+            found = true
+        }
+    }
+    console.log(changes.join("\n").trim())
+} catch (e) {
+    console.log('Error:', e.stack);
+    process.exit(1)
+}
--- .github/workflows/getChanges.sh
+++ .github/workflows/getChanges.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-version=$(./getVersion.sh)
-file="CHANGELOG.md"
-
-awk '/# 1.1.1/{flag=1;next}/#/{flag=0}flag' ../../$file
+CHANGES=$(node ./getChanges.js)
+echo $CHANGES
